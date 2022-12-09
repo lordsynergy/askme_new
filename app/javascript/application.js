b1b6296302abdb4071b5677f5935a6cebc7516c9
lordsynergy <djordj88@mail.ru>
2022-12-09 19:54:32 +0300

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,3 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "ROR/lesson49/my_askme/askme/app/javascript/controllers"
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'questions#index'
+
+  resources :questions
+
+  resources :questions do
+    member do
+      put 'hide'
+    end
+  end
+end
