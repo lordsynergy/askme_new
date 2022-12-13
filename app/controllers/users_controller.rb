ea771fcf92f90bcccedf8c4fb4826fc49ec2a9df
lordsynergy <djordj88@mail.ru>
2022-12-13 17:41:17 +0300

    add validate to user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   def new
+    session[:current_time] = Time.now
     @user = User.new
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
 
     @user = User.create(user_params)
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    if @user.save
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
 
-    session[:user_id] = @user.id
+      session[:user_id] = @user.id
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+
+      render :new
+    end
+
   end
 end
