88cdcb0c81b330f72a9271fb454df9d6b32bf7f9
lordsynergy <djordj88@mail.ru>
2022-12-13 15:57:47 +0300

    user authorization after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
-    User.create(user_params)
+    @user = User.create(user_params)
 
     redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+
+    session[:user_id] = @user.id
   end
 end
