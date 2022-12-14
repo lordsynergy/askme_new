47d0662782e72aff743d4fc75c7138f99a0ae2b2
lordsynergy <djordj88@mail.ru>
2022-12-14 20:08:12 +0300

    add validates for user and question

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w\-.]+@[\w\-]+\.[a-z]+\z/i }
+  validates :nickname, presence: true, uniqueness: true, length: { in: 3..40 }, format: { with: /\A\w+\z/ }
 
   def downcase_nickname
     nickname.downcase!
