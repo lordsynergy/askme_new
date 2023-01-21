5a8ccfbb394d5c585a40d195b41fdbbc1962f2d4
lordsynergy <djordj88@mail.ru>
2023-01-21 16:24:39 +0300

    fix hashtags

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,6 +2,6 @@ class Hashtag < ApplicationRecord
   has_many :question_hashtags, dependent: :destroy
   has_many :questions, through: :question_hashtags, source: :question
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :name, format: { with: /#[[:word:]-]+/ }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,26 +2,20 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
   has_many :question_hashtags, dependent: :destroy
-  has_many :hashtags, through: :question_hashtags, source: :hashtag
+  has_many :hashtags, through: :question_hashtags
 
   validates :body, presence: true, length: { maximum: 280 }
 
-  before_save :find_and_create_hashtags
-  after_commit :destroy_unused_tags
+  after_save_commit :find_and_create_hashtags
 
   private
 
   def find_and_create_hashtags
-    body_hashtags = self.body.scan(/#[[:word:]-]+/)
-    answer_hashtags = self.answer.to_s.scan(/#[[:word:]-]+/)
-    question_hashtags = body_hashtags | answer_hashtags
+    question_hashtags = "#{body}#{answer}".scan(/#[[:word:]-]+/)
 
     self.hashtags = question_hashtags.map do |tag|
-      Hashtag.find_or_create_by(name: tag)
+      Hashtag.create_or_find_by!(name: tag)
     end
   end
 
-  def destroy_unused_tags
-    Hashtag.left_outer_joins(:questions).where(questions: { id: nil }).destroy_all
-  end
 end
