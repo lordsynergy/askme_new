8a93df986dacc2a60721ceaaab872283b3de57ab
lordsynergy <djordj88@mail.ru>
2023-02-21 20:37:56 +0300

    not found for empty hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,11 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = Hashtag.find_by!(name: params[:name].downcase)
+
+    if @hashtag.questions.empty?
+      raise ActionController::RoutingError.new('Not Found')
+    else
+      @hashtag
+    end
   end
 end
