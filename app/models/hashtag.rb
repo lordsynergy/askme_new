e68f6903fb80239290e532d660d809aebc2b53b2
lordsynergy <djordj88@mail.ru>
2023-02-25 17:56:04 +0300

    fix hashtag model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,7 +2,7 @@ class Hashtag < ApplicationRecord
   REGEXP = /#[[:word:]-]+/
 
   has_many :question_hashtags, dependent: :destroy
-  has_many :questions, through: :question_hashtags, source: :question
+  has_many :questions, through: :question_hashtags
 
   validates :name, presence: true
 end
