c77beadaa1b45684c186310e3470376db2e9b872
lordsynergy <djordj88@mail.ru>
2023-02-28 00:46:04 +0300

    fix find for not empty hashtags

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,11 +1,5 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = Hashtag.find_by!(name: params[:name].downcase)
-
-    if @hashtag.questions.empty?
-      raise ActionController::RoutingError.new('Not Found')
-    else
-      @hashtag
-    end
+    @hashtag = Hashtag.joins(:questions).distinct.find_by!(name: params[:name].downcase)
   end
 end
