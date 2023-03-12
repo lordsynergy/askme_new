e6b0c1419861020bd0d9191051f9d2e47758b1b8
lordsynergy <djordj88@mail.ru>
2023-03-12 13:43:25 +0300

    add scoup for hashtag model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,6 +1,8 @@
 class Hashtag < ApplicationRecord
   REGEXP = /#[[:word:]-]+/
 
+  scope :with_questions, -> { where_exists(:question_hashtags) }
+
   has_many :question_hashtags, dependent: :destroy
   has_many :questions, through: :question_hashtags
 
